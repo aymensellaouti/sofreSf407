e06b0c7d35dd075f8b171d47beea964875f97740
aymen <aymen.sellaouti@gmail.com>
2019-07-15 14:55:49 +0100

    todo final

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
@@ -11,11 +12,9 @@ class TodoController extends AbstractController
     /**
      * @Route("/todos", name="todo")
      */
-    public function index(Request $request)
+    public function index(Request $request, SessionInterface $session)
     {
         // récupérer la session
-        $session = $request->getSession();
-        dump($session->has('todos'));
         // verifier si les todos existe dans la session
         if(! $session->has('todos')) {
             // si non
@@ -32,4 +31,75 @@ class TodoController extends AbstractController
         //Affichage de la liste des todos
         return $this->render('todo/index.html.twig');
     }
+
+    /**
+     * @Route("/todo/add/{name}/{content}", name="todo.add")
+     */
+    public function addTodo(Request $request, $name, $content) {
+        $session = $request->getSession();
+        if(! $session->has('todos')) {
+            $this->addFlash('error', 'Liste non encore initialisée');
+        } else {
+            $todos = $session->get('todos');
+            if(isset($todos[$name])){
+                $this->addFlash('error', "le todo $name existe déjà");
+            } else {
+                $todos[$name] = $content;
+                $session->set('todos', $todos);
+                $this->addFlash('success', "$name ajouté avec succès");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/todo/delete/{name}", name="todo.delete")
+     */
+    public function deleteTodo(Request $request, $name) {
+        $session = $request->getSession();
+        if(! $session->has('todos')) {
+            $this->addFlash('error', 'Liste non encore initialisée');
+        } else {
+            $todos = $session->get('todos');
+            if(! isset($todos[$name])){
+                $this->addFlash('error', "le todo $name n'existe pas");
+            } else {
+                unset($todos[$name]);
+                $session->set('todos', $todos);
+                $this->addFlash('success', "$name supprimé avec succès");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/todo/update/{name}/{content}", name="todo.update")
+     */
+    public function updateTodo(Request $request, $name, $content) {
+        $session = $request->getSession();
+        if(! $session->has('todos')) {
+            $this->addFlash('error', 'Liste non encore initialisée');
+        } else {
+            $todos = $session->get('todos');
+            if(! isset($todos[$name])){
+                $this->addFlash('error', "le todo $name n'existe pas");
+            } else {
+                $todos[$name] = $content;
+                $session->set('todos', $todos);
+                $this->addFlash('success', "$name mis à jour avec succès");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/todos/reset", name="todo.reset")
+     */
+    public function resetTodo(Request $request) {
+        $session = $request->getSession();
+        $session->clear();
+        $this->addFlash('success', "Les todos ont été réinitialisés avec succès");
+        return $this->redirectToRoute('todo');
+    }
+
 }
